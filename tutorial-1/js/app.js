b92ee763248aa8bfa1fe820e1d2a116fc56d39ea
John Robin <johnrobinlugtu@gmail.com>
2018-01-09 18:27:21 -0800

    created map

--- tutorial-1/js/app.js
+++ tutorial-1/js/app.js
@@ -1,4 +1,13 @@
+const width = 700;
+const height = 580;
+
+console.log(d3);
 // Wait for all dom elements to be loaded
 document.addEventListener('DOMContentLoaded', function(){
+    const svg = d3.select("body")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
 
+    const g = svg.append("g");
 });
